8b0ee7a8bc192b5ddb484eb59e2960a44396a66e
MOHAMMED OWAIS <150598658+MRSE435@users.noreply.github.com>
2026-01-08 16:43:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const PORT = process.env.PORT || 3000;
 const session = require('express-session');
 const app = express();
 app.use(express.json())
-app.use(cors({ origin:process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }))
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
+  ],
+  credentials: true
+}));
+
 app.use("/images", express.static(path.join(__dirname, "public")));
 
 app.set('trust proxy', 1);
